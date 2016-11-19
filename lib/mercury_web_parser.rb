59ceaf1f35da079b05b7590997dc975bbfaf8af7
Brendon Murphy <xternal1+github@gmail.com>
2016-11-19 13:46:36 -0800

    Switch out restclient for net/http

--- lib/mercury_web_parser.rb
+++ lib/mercury_web_parser.rb
@@ -1,19 +1,26 @@
 require "mercury_web_parser/version"
 require "json"
+require "net/https"
 require "ostruct"
-require "rest_client"
+require "uri"
 
 module MercuryWebParser
   API_ENDPOINT = "https://mercury.postlight.com/parser".freeze
 
   class << self
-    attr_accessor :api_token
+    attr_accessor :api_key
   end
 
   def self.parse(url)
-    resp = RestClient.get(API_ENDPOINT, params: { url: url },
-                          "x-api-key" => api_token, accept: :json)
+    uri = URI("#{API_ENDPOINT}?url=#{url}")
 
-    OpenStruct.new(JSON[resp])
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      request = Net::HTTP::Get.new(uri)
+      request["Accept"] = "application/json"
+      request["x-api-key"] = api_key
+      http.request(request)
+    end
+
+    OpenStruct.new(JSON[response.body])
   end
 end
